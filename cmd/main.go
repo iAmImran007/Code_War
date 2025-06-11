23456221ed02e3bdd8c19b72e03c9de4ae464def
Imran <skimran696969@gmail.com>
2025-06-11 17:31:37 +0530

    add cors origin req

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/iAmImran007/Code_War/pkg/database"
 	"github.com/iAmImran007/Code_War/pkg/routes"
-	
+	"github.com/gorilla/handlers"
 )
 
 func main() {
@@ -24,8 +24,30 @@ func main() {
 	// Initialize router with all routes
 	router := routes.NewRouter(&db)
 
+	//Configure CORS
+	corsOpts := handlers.CORS(
+		handlers.AllowedOrigins([]string{
+			"http://localhost:3000",  // React default
+			"http://localhost:3001",  // Alternative React port
+			"http://localhost:5173",  // Vite default
+			"http://localhost:4200",  // Angular default
+			"http://127.0.0.1:3000",  // Alternative localhost
+			"http://127.0.0.1:5173",  // Alternative localhost for Vite
+		}),
+		handlers.AllowedMethods([]string{
+			"GET", "POST", "PUT", "DELETE", "OPTIONS",
+		}),
+		handlers.AllowedHeaders([]string{
+			"Content-Type", 
+			"Authorization", 
+			"X-Requested-With",
+			"Accept",
+			"Origin",
+		}),
+		handlers.AllowCredentials(), // Important for auth cookies/tokens
+	)
 
 	//bind the cors
 	fmt.Println("Server running on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", router.Router))
+	log.Fatal(http.ListenAndServe(":8080", corsOpts(router.Router)))
 }
